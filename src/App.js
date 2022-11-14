c869a80b4f5149b1b9e555a39769dda949402886
Samuel <samuel1skultety@gmail.com>
2022-11-14 15:24:33 +0100

    feat: clarifai rest api integration

--- src/App.js
+++ src/App.js
@@ -1,18 +1,71 @@
+import {Component} from 'react';
 import ImageLinkForm from './components/ImageLinkForm';
 import Nav from './components/Nav';
 import ParticleBackground from './components/ParticleBackground';
 
-function App() {
-  return (
-    <div>
-      <Nav/>
-      <ImageLinkForm />
-      {/*
-  <FaceRecognition />*/}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+    }
+  }
 
-      <ParticleBackground id="tsparticles"/>
-    </div>
-  );
+  getRequestOptions = (url) => {
+   const raw = JSON.stringify({
+    "user_app_id": {
+      "user_id": "clarifai",
+      "app_id": "main"
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": url
+                }
+            }
+        }
+    ]
+  })
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key 5b697d940a5746d3ba96f1359114d187',
+    },
+    body: raw
+  }
+
+  return requestOptions;
+}
+
+  onInputChange = (event) => {
+    this.setState({imageUrl: event.target.value});
+    console.log(this.state.imageUrl);
+  }
+
+  onButtonSubmit = () => {
+    fetch(`https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`, this.getRequestOptions(this.state.imageUrl))
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav/>
+        <ImageLinkForm  onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        {/*
+    <FaceRecognition />*/}
+
+        <ParticleBackground id="tsparticles"/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm.jsx
+++ src/components/ImageLinkForm.jsx
@@ -1,16 +1,16 @@
 import Rank from './Rank';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
   return (
     <div className="flex flex-col bg-gray-500 bg-opacity-25 w-3/4 lg:w-2/4 rounded-xl mx-auto mt-2  backdrop-blur-sm shadow-xl">
       <Rank />
       <h3 className="mb-3 mt-4 mx-auto text-xl text-center font-medium text-gray-900 dark:text-white">Input an image URL for face recognition</h3>
-      <form action="">
+      
         <div className="flex mb-5 mx-3 justify-center">
-            <input type="text" className="w-3/4 h-10 px-3 rounded-l-xl focus:outline-none focus:ring-2 focus:ring-blue-300 focus:border-transparent" placeholder="Image URL" />
-            <button className="h-10 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-r-xl text-white px-5 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 select-none shadow-xl cursor-pointer">Detect</button>
+            <input onChange={onInputChange} type="text" className="w-3/4 h-10 px-3 rounded-l-xl focus:outline-none focus:ring-2 focus:ring-blue-300 focus:border-transparent" placeholder="Image URL" />
+            <button onClick={onButtonSubmit} className="h-10 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-r-xl text-white px-5 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 select-none shadow-xl cursor-pointer">Detect</button>
         </div>
-      </form>
+
     </div>
   );
 }
